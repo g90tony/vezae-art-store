811c5a21f324cdca7490851657b032c5d783e0b1
G90Tony <calebmbugua@gmail.com>
2022-02-25 18:37:19 +0300

    added app navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./ui/pages/company/landing";
+import AboutPage from "./ui/pages/company/about";
+import ContactsPage from "./ui/pages/company/contacts";
+import Error404Page from "./ui/pages/company/404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/about-us" element={<AboutPage />} />
+        <Route path="/contact-us" element={<ContactsPage />} />
+        <Route path="*" element={<Error404Page />} />
+      </Routes>
+    </Router>
   );
 }
 
