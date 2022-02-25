f3df37e9c5040f7d38739bf5a1e8f3c7f991d00f
G90Tony <calebmbugua@gmail.com>
2022-02-25 18:37:03 +0300

    added typography styles

---
+++ src/assets/styles/typography.js
@@ -0,0 +1,103 @@
+export const marketingTypographyStyles = {
+  hero: {
+    fontFamily: "'Playfair Display', serif",
+    fontSize: "72px",
+    fontWeight: 800,
+  },
+  subHero: {
+    fontFamily: "'Playfair Display', serif",
+    fontSize: "64px",
+    fontWeight: 800,
+  },
+  title: {
+    fontFamily: "'Playfair Display', serif",
+    fontSize: "48px",
+    fontWeight: 800,
+  },
+  subTitle: {
+    fontFamily: "'Playfair Display', serif",
+    fontSize: "32px",
+    fontWeight: 800,
+  },
+};
+
+export const headingTypographyStyles = {
+  h1: {
+    fontFamily: "28px",
+    fontSize: "",
+    fontWeight: 800,
+  },
+  h2: {
+    fontFamily: "'Playfair Display', serif",
+    fontSize: "24px",
+    fontWeight: 800,
+  },
+  h3: {
+    fontFamily: "'Playfair Display', serif",
+    fontSize: "22px",
+    fontWeight: 700,
+  },
+  h4: {
+    fontFamily: "'Playfair Display', serif",
+    fontSize: "20px",
+    fontWeight: 700,
+  },
+  h5: {
+    fontFamily: "'Playfair Display', serif",
+    fontSize: "18px",
+    fontWeight: 600,
+  },
+  h6: {
+    fontFamily: "'Playfair Display', serif",
+    fontSize: "16px",
+    fontWeight: 600,
+  },
+};
+
+export const bodyTypographyStyles = {
+  largeLight: {
+    fontFamily: "'Red Rose', cursive",
+    fontSize: "16px",
+    fontWeight: 400,
+  },
+  largeNormal: {
+    fontFamily: "'Red Rose', cursive",
+    fontSize: "16px",
+    fontWeight: 500,
+  },
+  largeBold: {
+    fontFamily: "'Red Rose', cursive",
+    fontSize: "16px",
+    fontWeight: 700,
+  },
+  defaultLight: {
+    fontFamily: "'Red Rose', cursive",
+    fontSize: "14px",
+    fontWeight: 804,
+  },
+  defaultNormal: {
+    fontFamily: "'Red Rose', cursive",
+    fontSize: "14px",
+    fontWeight: 500,
+  },
+  defaultBold: {
+    fontFamily: "'Red Rose', cursive",
+    fontSize: "14px",
+    fontWeight: 700,
+  },
+  smallLight: {
+    fontFamily: "'Red Rose', cursive",
+    fontSize: "12px",
+    fontWeight: 400,
+  },
+  smallNormal: {
+    fontFamily: "'Red Rose', cursive",
+    fontSize: "12px",
+    fontWeight: 500,
+  },
+  smallBold: {
+    fontFamily: "'Red Rose', cursive",
+    fontSize: "12px",
+    fontWeight: 700,
+  },
+};
