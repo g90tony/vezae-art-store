0bf4d4add12ba0ba85cc59b54767a0f662a80364
G90Tony <calebmbugua@gmail.com>
2022-02-25 21:41:26 +0300

    added grid container and  and included navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Grid } from "@mui/material";
+
 import LandingPage from "./ui/pages/company/landing";
 import AboutPage from "./ui/pages/company/about";
 import ContactsPage from "./ui/pages/company/contacts";
 import Error404Page from "./ui/pages/company/404";
+import NavBar from "./ui/modules/global/navbar";
 
+import { palette } from "./assets/styles/colors";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<LandingPage />} />
-        <Route path="/about-us" element={<AboutPage />} />
-        <Route path="/contact-us" element={<ContactsPage />} />
-        <Route path="*" element={<Error404Page />} />
-      </Routes>
-    </Router>
+    <Grid
+      container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh",
+        width: "100vw",
+        backgroundColor: palette.accentLight,
+      }}
+    >
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route path="/about-us" element={<AboutPage />} />
+          <Route path="/contact-us" element={<ContactsPage />} />
+          <Route path="*" element={<Error404Page />} />
+        </Routes>
+      </Router>
+    </Grid>
   );
 }
 
