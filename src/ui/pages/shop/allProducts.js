38c3d7427340eef741f971fbdb702806735a1f2b
G90Tony <calebmbugua@gmail.com>
2022-03-03 20:24:23 +0300

    added filter products card

--- src/ui/pages/shop/allProducts.js
+++ src/ui/pages/shop/allProducts.js
@@ -1,5 +1,40 @@
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import FilterProductsCard from "../../modules/shop/filterProductsCard";
 
 export default function ShopAllProductsPage() {
-  return <div>This is the shopall products page</div>;
+  return (
+    <Grid
+      container
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "nowrap",
+        minHeight: "100vh",
+        width: "100%",
+      }}
+    >
+      <Box
+        sx={{
+          width: { xs: "100%", lg: "25%" },
+          height: "100%",
+          display: "flex",
+          flexDirection: "columns",
+          justifyContent: "flex-start",
+        }}
+      >
+        <FilterProductsCard />
+      </Box>
+      <Box
+        sx={{
+          width: { xs: "100%", lg: "75%" },
+          height: "100%",
+          display: "flex",
+          flexDirection: "columns",
+          justifyContent: "flex-start",
+        }}
+      ></Box>
+    </Grid>
+  );
 }
