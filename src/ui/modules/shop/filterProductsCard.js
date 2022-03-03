8c83add88c71f49de85bbe8f49dcf581ef2f8ddd
G90Tony <calebmbugua@gmail.com>
2022-03-03 21:41:11 +0300

    reworked to lay nice with search bar

--- src/ui/modules/shop/filterProductsCard.js
+++ src/ui/modules/shop/filterProductsCard.js
@@ -133,7 +133,14 @@ export default function FilterProductsCard() {
   }
 
   return (
-    <Grid sx={{}}>
+    <Grid
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "flex-start",
+      }}
+    >
       <List
         sx={{
           padding: "10px",
@@ -564,7 +571,7 @@ export default function FilterProductsCard() {
         sx={{
           display: { xs: "none", lg: "flex" },
           flexDirection: "column",
-          marginTop: "40px",
+          marginTop: "10px",
           padding: "10px",
           width: "350px",
           backgroundColor: palette.secondary,
