256db56854933f95ad1ff7da0ab1030de006e473
G90Tony <calebmbugua@gmail.com>
2022-03-03 20:24:09 +0300

    created helper functions to handle filter functionality

---
+++ src/helpers/data/filterHelpers.js
@@ -0,0 +1,58 @@
+export function FilterAction(
+  data,
+  dataState,
+  selection,
+  updateSelection,
+  updateDataState
+) {
+  if (data.isChecked === false) {
+    const updatedSelection = selection;
+    updatedSelection.push(data);
+    updateSelection(updatedSelection);
+
+    data.isChecked = true;
+    dataState[data.indexNumber] = data;
+
+    updateDataState(dataState);
+  } else {
+    const selectedArtTypeIndex = selection.indexOf(data);
+    const updatedSelection = selection.splice(selectedArtTypeIndex, 1);
+
+    updateSelection(updatedSelection);
+
+    data.isChecked = false;
+    dataState[data.indexNumber] = data;
+
+    updateDataState(dataState);
+  }
+}
+
+export function FilterClear(
+  filterState,
+  filterStateUpdater,
+  filterToggleUpdater,
+  selectedFiltersStateUpdater
+) {
+  const resetCollectionState = filterState.map((col) => {
+    if (col.isChecked) {
+      col.isChecked = false;
+    }
+    return col;
+  });
+
+  filterStateUpdater(resetCollectionState);
+  filterToggleUpdater(false);
+  selectedFiltersStateUpdater([]);
+}
+
+export function FilterApply([
+  selectedCollections,
+  selectedArtTypes,
+  selectedCanvasSize,
+]) {
+  if (!selectedCollections && !selectedArtTypes && !selectedCanvasSize) {
+    alert("Filters are empty");
+  } else {
+    alert("please wait as will fetch the results");
+  }
+}
