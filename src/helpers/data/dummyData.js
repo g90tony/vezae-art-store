60eb47bd4f3fdca15bde374c550aa189c73b841a
G90Tony <calebmbugua@gmail.com>
2022-03-03 20:23:26 +0300

    created dummy data for filter card

---
+++ src/helpers/data/dummyData.js
@@ -0,0 +1,98 @@
+export const dummyCollectionsData = [
+  {
+    id: 1,
+    text: "Collection 1",
+    value: "collection_1",
+  },
+  {
+    id: 2,
+    text: "Collection 2",
+    value: "collection_2",
+  },
+  {
+    id: 3,
+    text: "Collection 3",
+    value: "collection_3",
+  },
+  {
+    id: 4,
+    text: "Collection 4",
+    value: "collection_4",
+  },
+  {
+    id: 5,
+    text: "Collection 5",
+    value: "collection_5",
+  },
+  {
+    id: 6,
+    text: "Collection 6",
+    value: "collection_6",
+  },
+];
+
+export const dummyArtTypesData = [
+  {
+    id: 1,
+    text: "Modernism",
+    value: "modernism",
+  },
+  {
+    id: 2,
+    text: "Impressionism",
+    value: "impressionism",
+  },
+  {
+    id: 3,
+    text: "Abstract Art",
+    value: "abstract_art",
+  },
+  {
+    id: 4,
+    text: "Expressionism",
+    value: "expressionism",
+  },
+  {
+    id: 5,
+    text: "Cubism",
+    value: "cubism",
+  },
+  {
+    id: 6,
+    text: "Surrealism",
+    value: "surrealism",
+  },
+];
+
+export const dummyCanvasSizesData = [
+  {
+    id: 1,
+    text: "12 inches by 12 inches",
+    value: "12x12",
+  },
+  {
+    id: 2,
+    text: "12 inches by 16 inches",
+    value: "12x16",
+  },
+  {
+    id: 3,
+    text: "16 inches by 16 inches",
+    value: "16x16",
+  },
+  {
+    id: 4,
+    text: "16 inches by 20 inches",
+    value: "16x20",
+  },
+  {
+    id: 5,
+    text: "18 inches by 24 inches",
+    value: "18x24",
+  },
+  {
+    id: 6,
+    text: "24 inches by 36 inches",
+    value: "24x36",
+  },
+];
