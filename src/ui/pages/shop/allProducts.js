8fdad7743fc752a27ad905d37f72c7c50d88d9b7
G90Tony <calebmbugua@gmail.com>
2022-03-05 00:59:15 +0300

    completed UI and responsiveness

--- src/ui/pages/shop/allProducts.js
+++ src/ui/pages/shop/allProducts.js
@@ -1,8 +1,12 @@
+import React from "react";
 import { Grid } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+
 import FilterProductsCard from "../../modules/shop/filterProductsCard";
 import ProductsSearchBar from "../../modules/shop/productsSearchBar";
+
+import { dummyProductsData as dummyProducts } from "../../../helpers/data/dummyData";
+import ProductItemCard from "../../components/productItemCard";
 
 export default function ShopAllProductsPage() {
   return (
@@ -14,8 +18,8 @@ export default function ShopAllProductsPage() {
         flexWrap: { xs: "wrap", lg: "nowrap" },
         minHeight: "100vh",
         width: "100%",
-        marginTop: "10px",
-        padding: "20px",
+        marginTop: "00px",
+        padding: "10px",
       }}
     >
       <Box
@@ -39,6 +43,25 @@ export default function ShopAllProductsPage() {
         }}
       >
         <ProductsSearchBar />
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            padding: "10px",
+          }}
+        >
+          {dummyProducts.map((product) => {
+            return (
+              <React.Fragment key={product.id}>
+                <ProductItemCard product={product} />
+              </React.Fragment>
+            );
+          })}
+        </Box>
       </Box>
     </Grid>
   );
