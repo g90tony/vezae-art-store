700df267cbdccb878106775d43162447fbaabcd1
G90Tony <calebmbugua@gmail.com>
2022-03-04 23:25:50 +0300

    moved logic to parent component

--- src/ui/components/priceSlider.js
+++ src/ui/components/priceSlider.js
@@ -4,30 +4,16 @@ import Slider from "@mui/material/Slider";
 import { bodyTypographyStyles } from "../../assets/styles/typography";
 import { Typography } from "@mui/material";
 
-export default function PriceSlider() {
-  const [value, setValue] = React.useState([0, 10000]);
-
-  const updatePrice = (event, newValue, activeThumb) => {
-    if (!Array.isArray(newValue)) {
-      return;
-    }
-
-    if (activeThumb === 0) {
-      setValue(newValue);
-    } else {
-      setValue(newValue);
-    }
-  };
-
+export default function PriceSlider(props) {
   return (
     <Box sx={{ width: 300 }}>
       <Typography sx={{ fontSize: bodyTypographyStyles.defaultBold }}>
-        Price Range : {value[0] * 100} - {value[1] * 100}
+        Price Range : {props.value[0] * 100} - {props.value[1] * 100}
       </Typography>
       <Slider
         getAriaLabel={() => "Price Slider"}
-        value={value}
-        onChange={updatePrice}
+        value={props.value}
+        onChange={props.updatePrice}
         valueLabelDisplay="auto"
         disableSwap
       />
