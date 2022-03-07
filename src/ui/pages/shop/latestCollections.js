007bad46629ce51f54d849c8069d96aa93682a5c
G90Tony <calebmbugua@gmail.com>
2022-03-07 15:47:02 +0300

    created filtered collections pages off all collections UI

---
+++ src/ui/pages/shop/latestCollections.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { dummyProductsData as dummyProducts } from "../../../helpers/data/dummyData";
+import CollectionsGridLayout from "../../layouts/collectionGridLayout";
+import CollectionsSearchBar from "../../modules/shop/collectionsSearchBar";
+import FilterCollectionsCard from "../../modules/shop/filterCollectionsCard";
+
+export default function ShopLatestCollectionsPage() {
+  return (
+    <CollectionsGridLayout
+      child1={<FilterCollectionsCard />}
+      child2={<CollectionsSearchBar pageName="Latest Collections" />}
+      collections={dummyProducts}
+      width="350px"
+      pageName="All Pieces"
+    />
+  );
+}
