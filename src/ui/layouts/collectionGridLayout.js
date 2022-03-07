b913b96c62006a9e36e9daa6bf788338222062e5
G90Tony <calebmbugua@gmail.com>
2022-03-07 15:44:38 +0300

    created collectionGridLayout off product grid layout UI

---
+++ src/ui/layouts/collectionGridLayout.js
@@ -0,0 +1,64 @@
+import { Box, Grid } from "@mui/material";
+import React from "react";
+import CollectionItemCard from "../components/collectionItemCard";
+
+export default function CollectionsGridLayout(props) {
+  return (
+    <Grid
+      container
+      sx={{
+        display: "flex",
+        flexDirection: { xs: "column", lg: "row" },
+        flexWrap: { xs: "wrap", lg: "nowrap" },
+        minHeight: "100vh",
+        width: "100%",
+        marginTop: "00px",
+        padding: "10px",
+      }}
+    >
+      <Box
+        sx={{
+          width: { xs: "100%", lg: "25%" },
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        {props.child1}
+      </Box>
+      <Box
+        sx={{
+          width: { xs: "100%", lg: "75%" },
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        {props.child2}
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: { xs: "center", lg: "start" },
+            alignItems: "flex-start",
+            width: "100%",
+            padding: "10px",
+            margin: "0 auto",
+          }}
+        >
+          {props.collections.map((product) => {
+            return (
+              <React.Fragment key={product.id}>
+                <CollectionItemCard product={product} width={props.width} />
+              </React.Fragment>
+            );
+          })}
+        </Box>
+      </Box>
+    </Grid>
+  );
+}
