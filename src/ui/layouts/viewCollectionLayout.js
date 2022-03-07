46fc5e7916e387aa42b6cb73425cb75b3eaefedf
G90Tony <calebmbugua@gmail.com>
2022-03-07 15:48:19 +0300

    completed view collection off product view UI

---
+++ src/ui/layouts/viewCollectionLayout.js
@@ -0,0 +1,78 @@
+import { Box, Breadcrumbs, Button, Grid } from "@mui/material";
+import React from "react";
+import { palette } from "../../assets/styles/colors";
+import { headingTypographyStyles } from "../../assets/styles/typography";
+import ChevronLeft from "@mui/icons-material/ChevronLeft";
+
+export default function ViewProductLayout(props) {
+  return (
+    <Grid
+      container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        flexWrap: "nowrap",
+        width: "100%",
+        padding: "10px",
+        margin: "0 auto",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          margin: "0 10px 10px 10px",
+        }}
+      >
+        <Breadcrumbs
+          aria-label="breadcrumb"
+          sx={{
+            color: palette.primary,
+            fontSize: headingTypographyStyles.h4,
+            marginLeft: "10px",
+          }}
+        >
+          <Button
+            underline="hover"
+            sx={{
+              color: palette.primary,
+              fontSize: headingTypographyStyles.h4,
+            }}
+            onClick={() => props.history(-1)}
+          >
+            <ChevronLeft
+              sx={{ margin: "auto", fontSize: headingTypographyStyles.h5 }}
+            />{" "}
+            Back
+          </Button>
+        </Breadcrumbs>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", lg: "row" },
+          justifyContent: "center",
+          flexWrap: "nowrap",
+          width: "100%",
+          padding: "10px",
+          margin: "0 auto",
+        }}
+      >
+        <Box sx={{ width: "100%", margin: "0 auto" }}>{props.child1}</Box>
+        <Box sx={{ width: "100%" }}>{props.child2}</Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: { xs: "column", lg: "row" },
+          width: "100%",
+          margin: "0 10px 10px 10px",
+        }}
+      >
+        {props.child3}
+      </Box>
+    </Grid>
+  );
+}
