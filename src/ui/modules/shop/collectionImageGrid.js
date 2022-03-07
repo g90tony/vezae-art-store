3dc9489e7522dc9c66cf89d780c66521fe969cf2
G90Tony <calebmbugua@gmail.com>
2022-03-07 17:05:17 +0300

    completed the collection image grid based off the landing collection section UI

---
+++ src/ui/modules/shop/collectionImageGrid.js
@@ -0,0 +1,37 @@
+import { Box, Grid } from "@mui/material";
+import React from "react";
+import CollectionGridItem from "../../components/collectionGridItem";
+
+export default function CollectionImageGrid(props) {
+  return (
+    <Grid
+      container
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: { xs: "100%", lg: "50% 50%" },
+          gridTemplateRows: "auto",
+          columnGap: { xs: "2.5px", lg: "5px" },
+          rowGap: { xs: "2.5px", lg: "5px" },
+        }}
+      >
+        {props.images.map((piece) => {
+          return (
+            <CollectionGridItem
+              key={props.images.indexOf(piece)}
+              product={piece}
+            />
+          );
+        })}
+      </Box>
+    </Grid>
+  );
+}
