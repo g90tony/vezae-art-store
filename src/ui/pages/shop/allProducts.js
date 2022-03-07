efe44148ca6b8ac1de5e0d8b699937681257bcc9
G90Tony <calebmbugua@gmail.com>
2022-03-07 15:05:34 +0300

    implemented all products layout

--- src/ui/pages/shop/allProducts.js
+++ src/ui/pages/shop/allProducts.js
@@ -1,70 +1,14 @@
 import React from "react";
-import { Grid } from "@mui/material";
-import { Box } from "@mui/system";
-
-import FilterProductsCard from "../../modules/shop/filterProductsCard";
-import ProductsSearchBar from "../../modules/shop/productsSearchBar";
 
 import { dummyProductsData as dummyProducts } from "../../../helpers/data/dummyData";
-import ProductItemCard from "../../components/productItemCard";
+import AllProductsLayout from "../../layouts/allProductsLayout";
 
 export default function ShopAllProductsPage() {
   return (
-    <Grid
-      container
-      sx={{
-        display: "flex",
-        flexDirection: { xs: "column", lg: "row" },
-        flexWrap: { xs: "wrap", lg: "nowrap" },
-        minHeight: "100vh",
-        width: "100%",
-        marginTop: "00px",
-        padding: "10px",
-      }}
-    >
-      <Box
-        sx={{
-          width: { xs: "100%", lg: "25%" },
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <FilterProductsCard />
-      </Box>
-      <Box
-        sx={{
-          width: { xs: "100%", lg: "75%" },
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          alignItems: "center",
-        }}
-      >
-        <ProductsSearchBar />
-
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            justifyContent: { xs: "center", lg: "start" },
-            alignItems: "flex-start",
-            width: "100%",
-            padding: "10px",
-            margin: "0 auto",
-          }}
-        >
-          {dummyProducts.map((product) => {
-            return (
-              <React.Fragment key={product.id}>
-                <ProductItemCard product={product} width={350} />
-              </React.Fragment>
-            );
-          })}
-        </Box>
-      </Box>
-    </Grid>
+    <AllProductsLayout
+      products={dummyProducts}
+      width="350px"
+      pageName="All Pieces"
+    />
   );
 }
