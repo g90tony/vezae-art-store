260e633d874de60f52efef47bc9ac065c438ac1e
G90Tony <calebmbugua@gmail.com>
2022-03-07 14:19:26 +0300

    cleared warnings

--- src/ui/pages/shop/viewProduct.js
+++ src/ui/pages/shop/viewProduct.js
@@ -43,7 +43,7 @@ export default function ShopViewProductPage() {
     return () => {
       setCurrentPiece(null);
     };
-  }, []);
+  }, [history, id]);
 
   function changeSizes(size) {
     setCurrentSize(size);
