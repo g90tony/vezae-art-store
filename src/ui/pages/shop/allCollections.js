716407268b4d763c16d321a2a56ea98896140086
G90Tony <calebmbugua@gmail.com>
2022-03-07 15:46:36 +0300

    implemented viewGrid layout

--- src/ui/pages/shop/allCollections.js
+++ src/ui/pages/shop/allCollections.js
@@ -1,5 +1,18 @@
 import React from "react";
 
+import { dummyProductsData as dummyProducts } from "../../../helpers/data/dummyData";
+import CollectionsGridLayout from "../../layouts/collectionGridLayout";
+import CollectionsSearchBar from "../../modules/shop/collectionsSearchBar";
+import FilterCollectionsCard from "../../modules/shop/filterCollectionsCard";
+
 export default function ShopAllCollectionsPage() {
-  return <div>This is the shop all collections page</div>;
+  return (
+    <CollectionsGridLayout
+      child1={<FilterCollectionsCard />}
+      child2={<CollectionsSearchBar pageName="All Collections" />}
+      collections={dummyProducts}
+      width="350px"
+      pageName="All Pieces"
+    />
+  );
 }
