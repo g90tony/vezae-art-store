0a7b2fa9e89bd66328ed5669404d55c617f665ca
G90Tony <calebmbugua@gmail.com>
2022-03-07 12:36:40 +0300

    moved landing grid item from to its own component

---
+++ src/ui/components/productGridItemCard.js
@@ -0,0 +1,63 @@
+import { Button, Typography } from "@mui/material";
+import React from "react";
+import { palette } from "../../assets/styles/colors";
+import { bodyTypographyStyles } from "../../assets/styles/typography";
+import "../../assets/styles/landingGridStyle.css";
+
+export default function ProductGridItemCard(props) {
+  return (
+    <div
+      className={`landing-grid-image${props.piece.id}`}
+      styles={{ width: props.width }}
+      key={props.piece.id}
+    >
+      <img
+        style={{
+          width: "100%",
+          height: "100%",
+          objectFit: "cover",
+          objectPosition: "center",
+        }}
+        src={props.piece.url}
+        alt={props.piece.title}
+      />
+      <div className="landing-grid-image-info">
+        <Typography
+          sx={{ fontSize: bodyTypographyStyles.largeBold, textAlign: "center" }}
+        >
+          {props.piece.title}
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: bodyTypographyStyles.defaultLight,
+            marginTop: "10px",
+            marginBottom: "15px",
+            textAlign: "center",
+          }}
+        >
+          {props.piece.price}
+        </Typography>
+        <Button
+          href={`/shop/pieces/view/${props.piece.id}`}
+          sx={{
+            backgroundColor: palette.secondary,
+            color: palette.primary,
+            "&:hover": {
+              backgroundColor: palette.primary,
+              color: palette.secondary,
+            },
+            borderRadius: 0,
+            fontSize: bodyTypographyStyles.smallBold,
+            textDecoration: "none",
+            textAlign: "center",
+            padding: "5px",
+            marginTop: "5px",
+            fontWeight: 900,
+          }}
+        >
+          View Piece
+        </Button>
+      </div>
+    </div>
+  );
+}
