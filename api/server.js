1eff7c7938225e63ca939c4b6d32cf8d3661ae9f
G90Tony <calebmbugua@gmail.com>
2022-03-10 18:59:31 +0300

    removed duplicate

--- api/server.js
+++ api/server.js
@@ -1,15 +1,10 @@
 import express from "express";
 
-const bodyParser = require("body-parser");
-
 const server = express();
 const PORT = process.env.PORT || 8000;
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
 
 server.listen(PORT, () => {
   console.log(`App running on Port number ${PORT}`);
