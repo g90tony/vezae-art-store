d8ba74c9eaf686d6c4d9e8783d43f978dfb19f4c
G90Tony <calebmbugua@gmail.com>
2022-03-12 14:27:02 +0300

    implemented UI

--- src/ui/pages/shop/checkout.js
+++ src/ui/pages/shop/checkout.js
@@ -1,5 +1,55 @@
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import { palette } from "../../../assets/styles/colors";
+import { cart } from "../../../helpers/data/dummyData";
+import CartCheckout from "../../components/cartCheckout";
+import CartPopup from "../../components/cartPopup";
+import CheckoutDetailsCard from "../../modules/shop/checkoutDetailsCard";
+import FilterProductsCard from "../../modules/shop/filterProductsCard";
 
 export default function ShopCheckoutPage() {
-  return <div>This is the shop checkout page</div>;
+  return (
+    <Grid
+      container
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        padding: "10px",
+        height: "80vh",
+        overflowY: "hidden",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: { xs: "100%", lg: "30.33%" },
+          margin: "0 auto 20px auto",
+          height: "75vh",
+          overflowY: "auto",
+          overflowX: "hidden",
+        }}
+      >
+        <CheckoutDetailsCard />
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          width: { xs: "100%", lg: "66.67%" },
+          height: "100%",
+          backgroundColor: palette.secondary,
+          // padding: "0 20px",
+        }}
+      >
+        <CartCheckout cart={cart} width="100%" />
+      </Box>
+    </Grid>
+  );
 }
