81940e0eb887d9385723ccc1adb2ed948a5c98ca
G90Tony <calebmbugua@gmail.com>
2022-04-06 05:18:50 +0300

    created product endpoint requests

---
+++ src/api/products.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const BASE_URL = `${process.env.REACT_APP_VEZAE_API_URL}`;
+
+async function getAllProducts() {
+  try {
+    const url = `${BASE_URL}/shop/products`;
+    const response = await axios.get(url);
+
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    console.error(error);
+
+    alert("There was a problem loading all the products");
+  }
+}
+
+async function getSingleProduct(productID) {
+  try {
+    const url = `${BASE_URL}/view/product/${productID}`;
+
+    const response = await axios.get(url);
+
+    if (response.status === 200) {
+      return response;
+    }
+  } catch (error) {
+    console.error(error);
+
+    alert("There was a problem loading the product");
+  }
+}
+
+export { getAllProducts, getSingleProduct };
