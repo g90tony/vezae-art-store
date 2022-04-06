f9defe27b59b53f889ded2da76d1fba7403634d5
G90Tony <calebmbugua@gmail.com>
2022-04-06 05:18:01 +0300

    updated  collections endpoints

---
+++ src/api/collections.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const BASE_URL = `${process.env.REACT_APP_VEZAE_API_URL}`;
+
+async function getAllCollections() {
+  try {
+    const url = `${BASE_URL}/shop/collections`;
+    const response = await axios.get(url);
+
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    console.error(error);
+
+    alert("There was a problem loading the collections");
+  }
+}
+
+async function getSingleCollection(collection_id) {
+  try {
+    const url = `${BASE_URL}/view/collection/${collection_id}`;
+
+    const response = await axios.get(url);
+
+    if (response.status === 200) {
+      // console.log(response.data);
+      return response.data;
+    }
+  } catch (error) {
+    console.error(error);
+
+    alert("There was a problem loading the collection");
+  }
+}
+export { getAllCollections, getSingleCollection };
