331bd6a8fbec3e8f92cbfa7b3f749ceb305df9fc
G90Tony <calebmbugua@gmail.com>
2022-04-06 05:31:00 +0300

    implemented api requests

--- src/ui/pages/shop/allProducts.js
+++ src/ui/pages/shop/allProducts.js
@@ -1,16 +1,39 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { getAllProducts } from "../../../api/products";
 
-import { dummyProductsData as dummyProducts } from "../../../helpers/data/dummyData";
 import ProductsGridLayout from "../../layouts/productsGridLayout";
 import FilterProductsCard from "../../modules/shop/filterProductsCard";
 import ProductsSearchBar from "../../modules/shop/productsSearchBar";
 
 export default function ShopAllProductsPage(props) {
   let { filter } = useParams();
+
+  const [products, setProducts] = React.useState([]);
+
+  const loadData = React.useCallback(async () => {
+    try {
+      const data = await getAllProducts();
+
+      if (data) {
+        setProducts(data);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    loadData();
+
+    return () => {
+      setProducts([]);
+    };
+  }, []);
+
   return (
     <ProductsGridLayout
-      products={dummyProducts}
+      products={products}
       width="350px"
       pageName={filter}
       child1={<FilterProductsCard />}
