b50ea61be14345ee31b7adc95f56f8f2c383c900
G90Tony <calebmbugua@gmail.com>
2022-04-06 05:29:50 +0300

    deleted redandant files

--- src/ui/pages/shop/filteredCollections.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ShopFilteredCollectionsPage() {
-  return <div>This is the shop filtered collections page</div>;
-}
--- src/ui/pages/shop/filteredProducts.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ShopFilteredProductsPage() {
-  return <div>This is the shop filtered products page</div>;
-}
