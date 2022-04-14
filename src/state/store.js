930341b2fae72129c1bac792a82f30d842934947
G90Tony <calebmbugua@gmail.com>
2022-04-14 16:11:19 +0300

    updated store due to slice changes

--- src/state/store.js
+++ src/state/store.js
@@ -1,16 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import collectionSlice from "./slices/collectionSlice";
-import collectionsSlice from "./slices/collectionsSlice";
-import productSlice from "./slices/productSlice";
+import cartSlice from "./slices/cartSlice";
+import currencySlice from "./slices/currencyCoversion";
+import userLocationSlice from "./slices/userLocation";
+import currencySelector from "./slices/currencySelector";
 import productsSlice from "./slices/productsSlice";
 import { searchSlice } from "./slices/searchSilce";
 
 export default configureStore({
   reducer: {
+    cart: cartSlice,
     products: productsSlice,
-    collections: collectionsSlice,
-    viewProduct: productSlice,
-    viewCollection: collectionSlice,
+    userLocation: userLocationSlice,
+    currencySelector: currencySelector,
+    currencies: currencySlice,
     searchResults: searchSlice,
   },
 });
