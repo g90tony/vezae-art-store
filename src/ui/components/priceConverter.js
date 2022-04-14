c2faf3c64e10662e0b6628fd84511a65c1ea789c
G90Tony <calebmbugua@gmail.com>
2022-04-14 19:37:13 +0300

    created price converter component

---
+++ src/ui/components/priceConverter.js
@@ -0,0 +1,30 @@
+import { Typography } from "@mui/material";
+import React from "react";
+import { useSelector } from "react-redux";
+import {
+  bodyTypographyStyles,
+  headingTypographyStyles,
+} from "../../assets/styles/typography";
+
+export default function PriceConverter(props) {
+  const selectedCurrency = useSelector(
+    (state) => state.currencySelector.selectedCurrency
+  );
+
+  const shopStyle = {
+    fontSize: bodyTypographyStyles.defaultBold,
+    textAlign: "start",
+  };
+
+  const viewStyle = { fontSize: headingTypographyStyles.h4, fontWeight: 700 };
+
+  return (
+    <Typography sx={props.view ? viewStyle : shopStyle}>
+      {`${selectedCurrency.symbol.toUpperCase()}`}{" "}
+      {props.selectedSize
+        ? Math.round(props.selectedSize.price * selectedCurrency.rate * 100) /
+          100
+        : "----.--"}
+    </Typography>
+  );
+}
