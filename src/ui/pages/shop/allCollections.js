6928ac4500045ff0c35243331e4774419bc3f790
G90Tony <calebmbugua@gmail.com>
2022-04-14 16:22:17 +0300

    implemented live data fetching

--- src/ui/pages/shop/allCollections.js
+++ src/ui/pages/shop/allCollections.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { getAllCollections } from "../../../api/collections";
 
 import { dummyProductsData as dummyProducts } from "../../../helpers/data/dummyData";
 import CollectionsGridLayout from "../../layouts/collectionGridLayout";
@@ -7,15 +8,33 @@ import CollectionsSearchBar from "../../modules/shop/collectionsSearchBar";
 import FilterCollectionsCard from "../../modules/shop/filterCollectionsCard";
 
 export default function ShopAllCollectionsPage(props) {
-  const { filter } = useParams();
+  const [collections, setCollections] = React.useState([]);
+
+  const loadData = React.useCallback(async () => {
+    try {
+      const data = await getAllCollections();
+
+      if (data) {
+        setCollections(data);
+      }
+    } catch (error) {
+      console.error("There was a problem loading all the collections", error);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    loadData();
+    return () => {
+      setCollections([]);
+    };
+  }, []);
 
   return (
     <CollectionsGridLayout
       child1={<FilterCollectionsCard />}
-      child2={<CollectionsSearchBar pageName={filter} />}
-      collections={dummyProducts}
+      child2={<CollectionsSearchBar />}
+      collections={collections}
       width="350px"
-      pageName={filter}
     />
   );
 }
