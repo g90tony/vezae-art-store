f0a6e3facfa27b72335321bc18ce56f51144c0cf
G90Tony <calebmbugua@gmail.com>
2022-05-02 06:28:38 +0300

    fixed localstorage bug with a localstorage resetter

--- src/state/store.js
+++ src/state/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, createNextState } from "@reduxjs/toolkit";
 import cartSlice from "./slices/cartSlice";
 import currencyRates from "./slices/currencyRates";
 import userLocationSlice from "./slices/userLocation";
@@ -11,11 +11,22 @@ function loadState(stateName, returnType) {
     return returnType;
   }
 
-  return localStorage.getItem(stateName)
-    ? JSON.parse(localStorage.getItem(stateName))
-    : returnType === "array"
-    ? []
-    : {};
+  try {
+    if (localStorage.getItem(stateName)) {
+      return JSON.parse(localStorage.getItem(stateName));
+    } else {
+      returnType === "array"
+        ? localStorage.setItem(stateName, JSON.stringify([]))
+        : localStorage.setItem(stateName, JSON.stringify({}));
+
+      return returnType === "array" ? [] : {};
+    }
+  } catch (error) {
+    console.log(`There was a problem setting up ${stateName}'s state`);
+    returnType === "array"
+      ? localStorage.setItem(stateName, JSON.stringify([]))
+      : localStorage.setItem(stateName, JSON.stringify({}));
+  }
 }
 
 const preloadedState = {
