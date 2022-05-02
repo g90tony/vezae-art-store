28659cbb719a6061e5dfe4f35d1d9b504d64f64e
G90Tony <calebmbugua@gmail.com>
2022-05-02 06:26:28 +0300

    fixed user location not found bug

--- src/api/useLocation.js
+++ src/api/useLocation.js
@@ -2,12 +2,22 @@ import axios from "axios";
 import { getCurrencyInfo } from "./currencyConverter";
 
 const getUseLocation = async () => {
-  const url = `https://geolocation-db.com/json/"`;
-  const response = await axios.get(url);
+  try {
+    const url = `https://geolocation-db.com/json/*`;
+    const response = await axios.get(url);
 
-  if (response.status === 200) {
-    const currency_info = await getCurrencyInfo(response.data.country_name);
-    return currency_info;
+    if (response.status === 200) {
+      // eslint-disable-next-line eqeqeq
+      if (response.data.country_code == "Not found") {
+        const currency_info = await getCurrencyInfo("kenya");
+        return currency_info;
+      } else {
+        const currency_info = await getCurrencyInfo(response.data.country_name);
+        return currency_info;
+      }
+    }
+  } catch (error) {
+    console.error("There was a problem loading the users location", error);
   }
 };
 
