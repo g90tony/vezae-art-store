4703f4634f450016bb6a55b01c74c455c396578d
G90Tony <calebmbugua@gmail.com>
2022-06-25 07:23:01 +0300

    changed from localstorage

--- src/ui/components/searchInput.js
+++ src/ui/components/searchInput.js
@@ -2,63 +2,105 @@ import * as React from "react";
 import { useNavigate } from "react-router-dom";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-import { useSelector } from "react-redux";
+
+import { getAllProducts } from "../../api/products";
+import { getAllCollections } from "../../api/collections";
 
 export default function SearchInput(props) {
   const history = useNavigate();
 
-  const ALL_PRODUCTS_STATE = useSelector((state) => state.products);
-  const ALL_COLLECTIONS_STATE = useSelector((state) => state.collections);
+  const [products, setProducts] = React.useState();
+  const [collections, setCollections] = React.useState();
+
+  const loadProductData = React.useCallback(async () => {
+    try {
+      const productData = await getAllProducts();
+
+      if (productData) {
+        setProducts(productData);
+      }
+    } catch (error) {
+      console.error("Error loading product data", error);
+    }
+  }, []);
+
+  const loadCollectionData = React.useCallback(async () => {
+    try {
+      const collectionData = await getAllCollections();
+
+      if (collectionData) {
+        setCollections(collectionData);
+      }
+    } catch (error) {
+      console.error("Error loading collection data", error);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    loadProductData();
+    loadCollectionData();
+
+    return () => {
+      setProducts();
+      setCollections();
+    };
+  }, [loadCollectionData, loadProductData]);
 
   const title = React.useCallback(() => {
-    const products_titles = ALL_PRODUCTS_STATE.map((product) => {
-      return {
-        product_id: product.product_id,
-        title: product.title,
-        firstLetter: product.title[0],
-      };
-    });
-    const collections_titles = ALL_COLLECTIONS_STATE.map((collection) => {
-      return {
-        collection_id: collection.collection_id,
-        title: collection.title,
-        firstLetter: collection.title[0],
-      };
-    });
+    if (products.length > 0 && collections.length > 0) {
+      let products_titles = products.map((product) => {
+        return {
+          product_id: product.product_id,
+          title: product.title,
+          firstLetter: product.title[0],
+        };
+      });
 
-    switch (props.searchType) {
-      case "products":
-        return products_titles;
-      case "collections":
-        return collections_titles;
+      let collections_titles = collections.map((collection) => {
+        return {
+          collection_id: collection.collection_id,
+          title: collection.title,
+          firstLetter: collection.title[0],
+        };
+      });
 
-      default:
-        const all_titles = [];
-        collections_titles.forEach((item) => {
-          item.title = `${item.title} (Collection)`;
+      switch (props.searchType) {
+        case "products":
+          return products_titles;
+        case "collections":
+          return collections_titles;
+        default:
+          const all_titles = [];
+          collections_titles.forEach((item) => {
+            item.title = `${item.title} (Collection)`;
 
-          all_titles.push(item);
-        });
+            all_titles.push(item);
+          });
 
-        products_titles.forEach((item) => {
-          item.title = `${item.title} (Product)`;
+          products_titles.forEach((item) => {
+            item.title = `${item.title} (Product)`;
 
-          all_titles.push(item);
-        });
-        return all_titles;
+            all_titles.push(item);
+          });
+          return all_titles;
+      }
     }
-  }, [ALL_COLLECTIONS_STATE, ALL_PRODUCTS_STATE, props.searchType]);
+  }, [collections, products, props.searchType]);
 
   const options = React.useCallback(() => {
-    const results = title().map((option) => {
-      const firstLetter = option.title[0].toUpperCase();
-      return {
-        firstLetter: /[0-9]/.test(firstLetter) ? "0-9" : firstLetter,
-        ...option,
-      };
-    });
-
-    return results.sort((a, b) => -b.firstLetter.localeCompare(a.firstLetter));
+    if (collections && products) {
+      let results;
+      results = title().map((option) => {
+        const firstLetter = option.title[0].toUpperCase();
+        return {
+          firstLetter: /[0-9]/.test(firstLetter) ? "0-9" : firstLetter,
+          ...option,
+        };
+      });
+      return results.sort(
+        (a, b) => -b.firstLetter.localeCompare(a.firstLetter)
+      );
+    }
   }, [title]);
 
   const label_text =
